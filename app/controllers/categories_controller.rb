ee5f8c5339eba00135491373a2300483cc10fbc5
kylejschulz <kylschulz@gmail.com>
2020-09-11 21:10:16 -0400

    Add routes and controls for create category and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,18 @@
 class CategoriesController < ApplicationController 
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+
+    else
+      render 'new'
+    end
 
   end
 
@@ -11,6 +23,12 @@ class CategoriesController < ApplicationController
   def index
 
   end
-  
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
 

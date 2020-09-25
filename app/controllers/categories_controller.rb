fe70909f89def4c53a72221cd8534469e9e193bc
kylejschulz <kylschulz@gmail.com>
2020-09-24 22:29:04 -0400

    finished the application, ready for deployment

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,7 +14,20 @@ class CategoriesController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
   end
 
   def show
